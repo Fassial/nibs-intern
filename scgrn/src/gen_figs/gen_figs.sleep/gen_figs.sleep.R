4d4395cefbdb52af533deea2be23e855affbc9cf
fassial <2508440340@qq.com>
2021-02-03 10:18:41 +0800

    scgrn: reshape struct of gen_figs in src

---
+++ scgrn/src/gen_figs/gen_figs.sleep/gen_figs.sleep.R
@@ -0,0 +1,12 @@
+###################################
+# Created on 09:49, Feb. 3rd, 2020
+# Author: fassial
+# Filenames: gen_figs.sleep.R
+###################################
+# dep
+library(Seurat)
+# local dep
+DIR.ROOT <- file.path(getwd(), "..", "..", "..")
+
+# macro
+
